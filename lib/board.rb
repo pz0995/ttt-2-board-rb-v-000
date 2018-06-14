db8c57508edf85c5ef3c8ebb985f27a4243784d3
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-06-14 02:25:38 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,12 @@
-row[1] = [" ", " ", " "]
-row[2] = [" ", " ", " "]
-row[3] = [" ", " ", " "]# Define the variable board below.
+row1 = [" ", " ", " "]
+row1[0]
+row1[1]
+row1[2]
+row2 = [" ", " ", " "]
+row2[3]
+row2[4]
+row2[5]
+row3 = [" ", " ", " "]# Define the variable board below.
+row3[6]
+row3[7]
+row3[8]
